547509a2cf8c7f1cbd8970ee2b59304d1249c8c1
finganapre <finganapre@gmail.com>
2018-02-20 16:51:15 +0500

    набросок страницы чата

---
+++ dist/js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+	
+});
+
+/*function openbox(id){
+    display = document.getElementById(id).style.display;
+    if(display=='block'){
+       document.getElementById(id).style.display='none';
+    }else{
+       document.getElementById(id).style.display='block';
+    }
+}
+*/
